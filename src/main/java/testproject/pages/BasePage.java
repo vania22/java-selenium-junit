2e2e5bc32821592d3274719720ffab3aaaa0eadb
Ivan Krupskyi <krupskiy111@gmail.com>
2022-02-06 20:31:37 +0000

    removing logging

--- src/main/java/testproject/pages/BasePage.java
+++ src/main/java/testproject/pages/BasePage.java
@@ -1,14 +1,11 @@
 package testproject.pages;
 
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import testproject.managers.LogManager;
 import testproject.managers.WaitManager;
 
 public class BasePage {
     protected WebDriver driver;
     protected WaitManager wait;
-    protected Logger log = LogManager.getLogger();
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
--- src/test/java/testproject/tests/BaseTest.java
+++ src/test/java/testproject/tests/BaseTest.java
@@ -1,25 +1,20 @@
 package testproject.tests;
 
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import testproject.managers.DriverManager;
-import testproject.managers.LogManager;
 
 public class BaseTest {
     protected WebDriver driver;
-    protected Logger log = LogManager.getLogger();
 
     @BeforeEach
     protected void setUp() {
-        log.info("Initialize driver before test");
         driver = new DriverManager().createDriverInstance();
     }
 
     @AfterEach()
     protected void tearDown() {
-        log.info("Destroy driver after test");
         driver.quit();
     }
 
